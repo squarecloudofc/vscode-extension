9c20826e2cbea0715ad75f20dea01bb16295147c
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2022-12-21 10:04:53 -0300

    New refreshStatus system

--- src/providers/AppsProvider.ts
+++ src/providers/AppsProvider.ts
@@ -1,54 +1,58 @@
+import {
+  ApplicationItem,
+  GenericTreeItem,
+  CustomTreeItem,
+  BaseProvider,
+  TreeItem,
+} from './BaseProvider';
+
 import CacheManager from '../managers/CacheManager';
-import { Application } from '@squarecloud/api';
+import { t } from 'vscode-ext-localisation';
 import * as pretty from 'pretty-ms';
-import * as vscode from 'vscode';
-import * as path from 'path';
 
-export class AppsProvider implements vscode.TreeDataProvider<TreeItem> {
-  private _onDidChangeTreeData: vscode.EventEmitter<
-    TreeItem | undefined | void
-  > = new vscode.EventEmitter<TreeItem | undefined | void>();
-
-  readonly onDidChangeTreeData: vscode.Event<TreeItem | undefined | void> =
-    this._onDidChangeTreeData.event;
-
+export class AppsProvider extends BaseProvider<TreeItem> {
   protected websiteOnly?: boolean;
 
   constructor(private cache: CacheManager) {
-    cache.refreshApps();
+    super();
 
-    cache.on('refreshApps', () => {
+    cache.on('refresh', () => {
+      this.refresh();
+    });
+
+    cache.on('refreshStatus', () => {
       this.refresh();
     });
   }
 
-  refresh(): void {
-    this._onDidChangeTreeData.fire();
-  }
-
-  getTreeItem(element: ApplicationItem): vscode.TreeItem {
-    return element;
-  }
-
   async getChildren(element?: TreeItem): Promise<TreeItem[]> {
+    const { contextValue } = element || {};
+
     if (
-      element?.contextValue === 'application' &&
+      (contextValue === 'square-bot' || contextValue === 'square-site') &&
       element instanceof ApplicationItem
     ) {
-      const { status } = element;
+      const status = await this.cache.status.get(element.app.id)!;
 
       return [
         new GenericTreeItem(
           'Uptime',
-          0,
           'uptime',
           pretty(Date.now() - status.uptimeTimestamp, { compact: true })
         ),
 
-        new GenericTreeItem('CPU', 0, 'cpu', status.cpuUsage),
-        new GenericTreeItem('RAM', 0, 'ram', status.ramUsage + 'MB'),
-        new GenericTreeItem('Network', 0, 'network', status.network.now),
-        new GenericTreeItem('Storage', 0, 'storage', status.storageUsage),
+        new GenericTreeItem('CPU', 'cpu', status.cpuUsage),
+        new GenericTreeItem('RAM', 'ram', status.ramUsage + 'MB'),
+        new GenericTreeItem(
+          t('generic.network'),
+          'network',
+          status.network.now
+        ),
+        new GenericTreeItem(
+          t('generic.storage'),
+          'storage',
+          status.storageUsage
+        ),
       ];
     }
 
@@ -56,93 +60,36 @@ export class AppsProvider implements vscode.TreeDataProvider<TreeItem> {
 
     if (!applications?.length) {
       if (!this.cache.api) {
-        return [new GenericTreeItem('No API key provided.', 0, 'error')];
+        return [
+          new CustomTreeItem(
+            'Click here to set an API key.',
+            {
+              command: 'squarecloud.setApiKey',
+              title: '%command.setApiKey%',
+            },
+            'warning'
+          ),
+        ];
       }
 
-      return [new GenericTreeItem('Loading...', 0, 'ripple')];
+      return [
+        new GenericTreeItem(
+          t('generic.loading'),
+          'ripple',
+          undefined,
+          'loading'
+        ),
+      ];
     }
 
     applications = applications.filter(({ isWebsite }) => {
       return this.websiteOnly ? isWebsite : !isWebsite;
     });
 
-    return await Promise.all(
-      applications.map(
-        async (app) => new ApplicationItem(app, await app.getStatus())
-      )
-    );
-  }
-}
-
-export class GenericTreeItem extends vscode.TreeItem {
-  constructor(
-    public readonly label: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState = 0,
-    public readonly iconName?: string,
-    public readonly description?: string
-  ) {
-    super(label, collapsibleState);
-    this.description = description;
-  }
-
-  iconPath = {
-    light: path.join(
-      __dirname,
-      '..',
-      '..',
-      'resources',
-      'light',
-      `${this.iconName}.svg`
-    ),
-    dark: path.join(
-      __dirname,
-      '..',
-      '..',
-      'resources',
-      'dark',
-      `${this.iconName}.svg`
-    ),
-  };
-}
-
-export class ApplicationItem extends vscode.TreeItem {
-  constructor(
-    public readonly app: Application,
-    public readonly status: ThenArg<ReturnType<Application['getStatus']>>,
-    public readonly command?: vscode.Command
-  ) {
-    super(
-      app.tag,
-      status.running
-        ? vscode.TreeItemCollapsibleState.Collapsed
-        : vscode.TreeItemCollapsibleState.None
+    const applicationItems = applications.map(
+      (app) => new ApplicationItem(app, this.cache)
     );
 
-    this.tooltip = `${app.id}`;
+    return applicationItems;
   }
-
-  iconPath = {
-    light: path.join(
-      __dirname,
-      '..',
-      '..',
-      'resources',
-      'light',
-      this.status.running ? 'app-online.svg' : 'app-offline.svg'
-    ),
-    dark: path.join(
-      __dirname,
-      '..',
-      '..',
-      'resources',
-      'dark',
-      this.status.running ? 'app-online.svg' : 'app-offline.svg'
-    ),
-  };
-
-  contextValue = 'application';
 }
-
-type TreeItem = ApplicationItem | GenericTreeItem;
-
-type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
