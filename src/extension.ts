2cf3090d7b40f937096ff6cbd008ab81d0a2b9d5
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2022-12-21 10:04:10 -0300

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,8 @@ process.on('uncaughtException', console.error);
 process.on('unhandledRejection', console.error);
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Extension loaded.');
+
   new SquareCloud(context);
 }
 
