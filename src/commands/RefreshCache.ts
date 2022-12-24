ad3717c1a6a1887e245a34191ba40b24b5c49b20
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2022-12-24 20:22:23 -0300

    Update RefreshCache.ts

--- src/commands/RefreshCache.ts
+++ src/commands/RefreshCache.ts
@@ -1,17 +1,5 @@
 import { Command } from '../structures/Command';
-import { t } from 'vscode-ext-localisation';
-import * as vscode from 'vscode';
 
-export default new Command('refreshCache', async (ctx) => {
-  vscode.window.withProgress(
-    {
-      location: vscode.ProgressLocation.Window,
-      title: t('generic.refreshing'),
-    },
-    async (progress) => {
-      await ctx.cache.refresh();
-
-      progress.report({ increment: 100 });
-    }
-  );
+export default new Command('refreshCache', (ctx) => {
+  ctx.cache.refresh();
 });
