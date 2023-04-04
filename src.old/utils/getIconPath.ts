f9e8d12e012b5ac2c1e97b5897ccddda66285acd
João Gabriel <64219777+bluee-js@users.noreply.github.com>
2023-04-04 18:41:36 -0300

    Delete src.old directory

--- src.old/utils/getIconPath.ts
+++
@@ -1,8 +0,0 @@
-import * as path from 'path';
-
-export default function getIconPath(iconName: string) {
-  return {
-    light: path.join(__dirname, '..', '..', 'resources', 'light', iconName),
-    dark: path.join(__dirname, '..', '..', 'resources', 'dark', iconName),
-  };
-}
