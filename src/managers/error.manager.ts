2dd5df0c80cdec37fc06b0fe3b2663bc21db1e53
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2023-04-07 02:33:35 -0300

    New error manager

---
+++ src/managers/error.manager.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+import { t } from 'vscode-ext-localisation';
+
+interface CommonError {
+  message: string;
+  code: string;
+}
+
+class ErrorManager {
+  handleError(error: CommonError, ...args: any[]) {
+    console.error(error, ...args);
+
+    if ('message' in error && 'code' in error) {
+      this.throwVsCodeError(error);
+    }
+
+    return undefined;
+  }
+
+  throwVsCodeError(error: CommonError) {
+    vscode.window.showErrorMessage(t('generic.error', { ...error }));
+  }
+}
+
+export default new ErrorManager();
