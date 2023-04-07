d78b8fffad2874c8bf8b2ebe2d1a385264dc1710
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2023-04-07 02:33:55 -0300

    New setApiKey method

--- src/squarecloud.ts
+++ src/squarecloud.ts
@@ -1,8 +1,14 @@
 import * as vscode from 'vscode';
 import commandManager from './managers/command.manager';
+import squareApiManager from './managers/squarecloud-api.manager';
 
 export class SquareCloud {
   constructor(public readonly context: vscode.ExtensionContext) {
     commandManager.loadCommands(this);
+  }
+
+  setApiKey(key: string) {
+    squareApiManager.setApiKey(key);
+    return this;
   }
 }
