ba5bf8ff1b4695d65b4a4558538a43342656bfb7
Gabriel <64219777+bluee-js@users.noreply.github.com>
2023-04-11 12:19:02 -0300

    Load configManager

--- src/squarecloud.ts
+++ src/squarecloud.ts
@@ -1,13 +1,14 @@
 import * as vscode from 'vscode';
+import baritemManager from './managers/baritem.manager';
+import cacheManager from './managers/cache.manager';
 import commandManager from './managers/command.manager';
+import configManager from './managers/config.manager';
 import translationManager from './managers/translation.manager';
 import treeviewManager from './managers/treeview.manager';
-import './managers/config.manager';
-import cacheManager from './managers/cache.manager';
-import baritemManager from './managers/baritem.manager';
 
 export class SquareCloud {
   constructor(public readonly context: vscode.ExtensionContext) {
+    configManager.setUpListeners();
     commandManager.loadCommands(this);
     translationManager.loadTranslations(context);
     baritemManager.loadBarItem(context);
