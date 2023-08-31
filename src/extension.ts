9180ce7257975699a5788f5da96880aaeac51620
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2023-08-31 20:04:30 -0300

    handle error properly

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,9 @@
 import * as vscode from 'vscode';
 import { SquareCloud } from './squarecloud';
+import errorManager from './managers/error.manager';
 
-process.on('uncaughtException', console.error);
-process.on('unhandledRejection', console.error);
+process.on('uncaughtException', errorManager.handleError);
+process.on('unhandledRejection', errorManager.handleError);
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('[Square Cloud Easy] Extension loaded.');
