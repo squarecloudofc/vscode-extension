57f5c025499f4b200df45f08aa8a99c091e8d42e
João Gabriel Tonaco <blu3zin@gmail.com>
2023-11-22 07:52:45 -0300

    refactor(treeviews): rename tree items at package

--- src/core/activate.ts
+++ src/core/activate.ts
@@ -1,8 +1,12 @@
 import * as vscode from "vscode";
 import { getVscodeLang, loadTranslations } from "vscode-ext-localisation";
+import { TreeViewsManager } from "../treeviews/manager";
 
 export function setUpExtension(context: vscode.ExtensionContext, lang: string) {
   loadTranslations(lang, context.extensionPath);
+
+  const treeViews = new TreeViewsManager();
+  treeViews.registerTreeViews();
 }
 
 export function activate(context: vscode.ExtensionContext): void {
