7773e6bd131470aeadad16fb12cd5032589b121d
João Gabriel Tonaco <blu3zin@gmail.com>
2023-11-22 07:54:18 -0300

    refactor(store): adjustments to applications store

--- src/store/applications.ts
+++ src/store/applications.ts
@@ -1,7 +1,7 @@
 import { Application, SimpleApplicationStatus } from "@squarecloud/api";
 import { Store } from ".";
 
-export interface ApplicationsState {
+export interface ApplicationsStore {
   statuses: SimpleApplicationStatus[];
   applications: Application[];
   favorited: string[];
@@ -14,20 +14,20 @@ export interface ApplicationsState {
   isFavorited(applicationId: string): boolean;
 }
 
-export default new Store<ApplicationsState>(({ get, set }) => ({
+export default new Store<ApplicationsStore>(({ get, set }) => ({
   applications: [],
   statuses: [],
   favorited: [],
 
-  setApplications: (applications: Application[]) => set({ applications }),
-  setStatuses: (statuses: SimpleApplicationStatus[]) => set({ statuses }),
-  toggleFavorite: (applicationId: string) =>
+  setApplications: (applications) => set({ applications }),
+  setStatuses: (statuses) => set({ statuses }),
+  toggleFavorite: (applicationId) =>
     set({
       favorited: get().favorited.includes(applicationId)
         ? get().favorited.filter((id) => id !== applicationId)
         : [...get().favorited, applicationId],
     }),
 
-  getStatus: (applicationId: string) => get().statuses.find((status) => status.applicationId === applicationId),
-  isFavorited: (applicationId: string) => get().favorited.includes(applicationId),
+  getStatus: (applicationId) => get().statuses.find((status) => status.applicationId === applicationId),
+  isFavorited: (applicationId) => get().favorited.includes(applicationId),
 }));
