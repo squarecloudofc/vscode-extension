9eba9ac5d5dafe53c932616ec6ca31bed123a30c
João Gabriel Tonaco <blu3zin@gmail.com>
2023-11-27 13:03:00 -0300

    refactor(listeners): implement api key testing at config listener

--- src/listeners/config.ts
+++ src/listeners/config.ts
@@ -1,11 +1,12 @@
 import { workspace } from "vscode";
-import { treeViewsManager } from "../treeviews/manager";
 import { CONFIG_API_KEY } from "../constants/config";
+import { applicationsManager } from "../api/applications";
+import { coreConfig } from "../config/core";
 
 export function listenForApiKey() {
-  return workspace.onDidChangeConfiguration((event) => {
-    if (event.affectsConfiguration(CONFIG_API_KEY)) {
-      treeViewsManager.refreshAll();
+  return workspace.onDidChangeConfiguration(async (event) => {
+    if (event.affectsConfiguration(CONFIG_API_KEY) && (await coreConfig.testApiKey())) {
+      await applicationsManager.refresh();
     }
   });
 }
