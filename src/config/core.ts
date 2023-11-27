e62cd6d67c489497773ad9f5e8541c435c6c6dba
João Gabriel Tonaco <blu3zin@gmail.com>
2023-11-27 12:58:15 -0300

    feat(config): set and test api key methods

--- src/config/core.ts
+++ src/config/core.ts
@@ -1,3 +1,4 @@
+import { SquareCloudAPI } from "@squarecloud/api";
 import { workspace } from "vscode";
 
 class CoreConfig {
@@ -7,6 +8,25 @@ class CoreConfig {
 
   get apiKey() {
     return this.rootConfiguration.get<string | undefined>("apiKey");
+  }
+
+  setApiKey(value?: string) {
+    this.rootConfiguration.update("apiKey", value, true);
+  }
+
+  async testApiKey() {
+    const apiKey = this.apiKey;
+
+    if (!apiKey) {
+      return this.setApiKey(undefined);
+    }
+
+    const api = new SquareCloudAPI(apiKey);
+
+    return api.users
+      .get()
+      .then(() => apiKey)
+      .catch(() => this.setApiKey(undefined));
   }
 }
 
