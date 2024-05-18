b93937d48017ab778d1c502e3ab13fe880911cba
João Gabriel Tonaco <blu3zin@gmail.com>
2024-05-17 23:01:13 -0300

    refactor(core): handle config secrets and store persist

--- src/core/activate.ts
+++ src/core/activate.ts
@@ -1,5 +1,6 @@
 import type { ExtensionContext } from "vscode";
 import { getVscodeLang, loadTranslations } from "vscode-ext-localisation";
+import { coreConfig } from "../config/core";
 import { listenersManager } from "../listeners/manager";
 import applicationsStore from "../store/applications";
 import { treeViewsManager } from "../treeviews/manager";
@@ -7,11 +8,13 @@ import { treeViewsManager } from "../treeviews/manager";
 export async function setUpExtension(context: ExtensionContext, lang: string) {
 	console.log("[Square Cloud Easy] Starting!");
 
+	coreConfig.setSecretStorage(context.secrets);
 	loadTranslations(lang, context.extensionPath);
 	listenersManager.register(context);
 	treeViewsManager.register();
 
 	applicationsStore.subscribe(() => treeViewsManager.refreshAll());
+	applicationsStore.persist({ name: "applications", context });
 }
 
 export async function activate(context: ExtensionContext): Promise<void> {
