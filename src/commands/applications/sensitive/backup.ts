01e2a6c8b7a1accfa6090e8e377d204fab1d0bf7
João Gabriel Tonaco User <joao.tonaco@proton.me>
2024-07-26 00:47:57 -0300

    feat: app backup command

---
+++ src/commands/applications/sensitive/backup.ts
@@ -0,0 +1,43 @@
+import { writeFile } from "node:fs/promises";
+import { join } from "node:path";
+import { ApplicationCommand } from "@/structures/application/command";
+import { ProgressLocation, window } from "vscode";
+import { t } from "vscode-ext-localisation";
+
+export default new ApplicationCommand(
+	"backupEntry",
+	async (extension, { application }) => {
+		if (extension.api.paused) {
+			return;
+		}
+
+		const dialog = await window.showOpenDialog({
+			canSelectFolders: true,
+			openLabel: t("backup.save"),
+			title: `Backup - ${application.name}`,
+		});
+
+		if (!dialog) {
+			return;
+		}
+
+		const [{ fsPath }] = dialog;
+
+		window.withProgress(
+			{
+				location: ProgressLocation.Notification,
+				title: t("backup.loading"),
+			},
+			async (progress) => {
+				const buffer = await extension.api.pauseUntil(() =>
+					application.backups.download(),
+				);
+
+				await writeFile(join(fsPath, `backup-${application.id}.zip`), buffer);
+
+				window.showInformationMessage(t("backup.loaded"));
+				progress.report({ increment: 100 });
+			},
+		);
+	},
+);
