cf1000c2c1d0af8506ac8e90d56e5b4b91f77c1f
josejooj <josejooj.dev@gmail.com>
2025-04-05 01:03:01 -0300

    feat: create DISPLAY_NAME parameter definition

---
+++ src/config-file/parameters/display-name.ts
@@ -0,0 +1,17 @@
+import { createDiagnostic } from "@/lib/utils/diagnostic";
+import type { ConfigFileParameter } from "@/types/config-file";
+import { t } from "vscode-ext-localisation";
+
+export const DISPLAY_NAME = {
+	validation(keys, value, line, diagnostics, document) {
+		if (value.length > 32) {
+			diagnostics.push(
+				createDiagnostic(
+					document,
+					line,
+					t("configFile.error.long.displayName"),
+				),
+			);
+		}
+	},
+} satisfies ConfigFileParameter;
