61456e1e029c433fa933a042799b3d680da55c61
josejooj <josejooj.dev@gmail.com>
2025-04-05 01:02:51 -0300

    feat: create DESCRIPTION parameter definition

---
+++ src/config-file/parameters/description.ts
@@ -0,0 +1,17 @@
+import { createDiagnostic } from "@/lib/utils/diagnostic";
+import type { ConfigFileParameter } from "@/types/config-file";
+import { t } from "vscode-ext-localisation";
+
+export const DESCRIPTION = {
+	validation(keys, value, line, diagnostics, document) {
+		if (value.length > 280) {
+			diagnostics.push(
+				createDiagnostic(
+					document,
+					line,
+					t("configFile.error.long.description"),
+				),
+			);
+		}
+	},
+} satisfies ConfigFileParameter;
