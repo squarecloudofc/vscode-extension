08f70720b9658459477313a0df889e5c10f017a5
josejooj <josejooj.dev@gmail.com>
2025-04-05 01:02:45 -0300

    feat: create AUTORESTART parameter definition

---
+++ src/config-file/parameters/autorestart.ts
@@ -0,0 +1,24 @@
+import { createDiagnostic } from "@/lib/utils/diagnostic";
+import type { ConfigFileParameter } from "@/types/config-file";
+import * as vscode from "vscode";
+import { t } from "vscode-ext-localisation";
+
+export const AUTORESTART = {
+	validation(keys, value, line, diagnostics, document) {
+		if (!["true", "false"].includes(value)) {
+			diagnostics.push(
+				createDiagnostic(
+					document,
+					line,
+					t("configFile.error.invalid.autoRestart"),
+				),
+			);
+		}
+	},
+	autocomplete(document, position) {
+		return ["true", "false"].map(
+			(value) =>
+				new vscode.CompletionItem(value, vscode.CompletionItemKind.EnumMember),
+		);
+	},
+} satisfies ConfigFileParameter;
