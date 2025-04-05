3c971d6dc7f13a480f7bd1bdf15f8674dcaa731d
josejooj <josejooj.dev@gmail.com>
2025-04-05 01:03:59 -0300

    feat: export all parameters

---
+++ src/config-file/parameters.ts
@@ -0,0 +1,19 @@
+import { AUTORESTART } from "./parameters/autorestart";
+import { DESCRIPTION } from "./parameters/description";
+import { DISPLAY_NAME } from "./parameters/display-name";
+import { MAIN } from "./parameters/main";
+import { MEMORY } from "./parameters/memory";
+import { START } from "./parameters/start";
+import { SUBDOMAIN } from "./parameters/subdomain";
+import { VERSION } from "./parameters/version";
+
+export const ConfigFileParameters = {
+	MAIN,
+	VERSION,
+	AUTORESTART,
+	SUBDOMAIN,
+	DISPLAY_NAME,
+	DESCRIPTION,
+	START,
+	MEMORY,
+};
