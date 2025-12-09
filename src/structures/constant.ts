4ad5f677499e33564826d125f504c7e9674a970a
João Gabriel <joao.tonaco@proton.me>
2025-12-09 16:47:46 -0300

    refactor(constant): add readonly value property to avoid repeated string interpolation

--- src/structures/constant.ts
+++ src/structures/constant.ts
@@ -1,4 +1,6 @@
 export class Constant<const T extends string, const U extends string> {
+  public readonly value: `${U}.${T}`;
+
   /**
    * Constructs a new instance of the Constant class.
    *
@@ -8,9 +10,11 @@ export class Constant<const T extends string, const U extends string> {
   constructor(
     public readonly name: T,
     public readonly id: U,
-  ) {}
+  ) {
+    this.value = `${id}.${name}`;
+  }
 
   toString() {
-    return `${this.id}.${this.name}` as const;
+    return this.value;
   }
 }
